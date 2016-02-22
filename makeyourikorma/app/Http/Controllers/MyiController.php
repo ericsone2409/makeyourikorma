57901f2005ac3e8efa1a7cdf897a3ed839603d27
ericsone2409 <ericsone2409@gmail.com>
2016-02-21 22:09:52 -0430

    agregue una carpeta con la vista del MYI, la puse adentro de la carpta extra

---
+++ makeyourikorma/app/Http/Controllers/MyiController.php
@@ -0,0 +1,15 @@
+<?php namespace App\Http\Controllers;
+
+use Image;
+use App\UniqueImage;
+use App\Imagen;
+use App\Color;
+class MyiController extends Controller {
+
+	
+	public function index()
+	{
+		return view('welcome'); //aqui hay que poner la vista del MYI
+	}
+
+}
